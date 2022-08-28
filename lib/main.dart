61ca903b0ea92b63e7083ce5a6ee660795f3964c
bispoalef <alef.b.dos.santos.1993@gmail.com>
2022-08-28 16:49:25 -0300

    adicionando provider ao projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:starwars/data/api_service.dart';
 import 'package:starwars/screens/pagina_inicial.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ApiService(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
