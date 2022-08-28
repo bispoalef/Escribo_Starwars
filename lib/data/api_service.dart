d5885de0808312e3f338a0ed0ad74c0cde5c5587
bispoalef <alef.b.dos.santos.1993@gmail.com>
2022-08-28 16:59:32 -0300

    iniciando requisição da api com http

--- lib/data/api_service.dart
+++ lib/data/api_service.dart
@@ -1,5 +1,26 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
-class ApiService extends ChangeNotifier{
+class ApiService extends ChangeNotifier {
+  static const url_base = 'https://swapi.dev/api/';
 
+  List<dynamic> listaDeNomes = [];
+
+  carregarDados() async {
+    await _buscarNomes();
+  }
+
+  _buscarNomes() async {
+    var url = Uri.parse('$url_base/people');
+
+    var response = await http.get(url);
+
+    final jsonResponse = jsonDecode(response.body);
+    final List<dynamic> lista = jsonResponse['results'];
+    for (var element in lista) {
+      listaDeNomes.add(element['name']);
+    }
+  }
 }
